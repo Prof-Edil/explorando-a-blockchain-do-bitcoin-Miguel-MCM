ab68f9d1ec6bf365436ec7149fbb3fb753afa43b
Miguel-MCM <miguelcarvalhodemedeiros.mcm@gmail.com>
2024-11-11 14:12:43 -0300

    tr

--- submission/004.sh
+++ submission/004.sh
@@ -1,6 +1,6 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-descinfo=$(bitcoin-cli getdescriptorinfo  "wpkh(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)")
+descinfo=$(bitcoin-cli getdescriptorinfo  "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)")
 checksum=$(echo $descinfo | jq .checksum | xargs)
-list=$(bitcoin-cli deriveaddresses "wpkh(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#${checksum:0:-1}" [100,100])
+list=$(bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#${checksum:0:-1}" [100,100])
 echo $list | jq '.[0]'
