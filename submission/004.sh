c25f93c4d4ebff0b75667dc565a0dfc7e031aae8
Miguel-MCM <98604253+Miguel-MCM@users.noreply.github.com>
2024-11-11 14:14:52 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -2,5 +2,5 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 descinfo=$(bitcoin-cli getdescriptorinfo  "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)")
 checksum=$(echo $descinfo | jq .checksum | xargs)
-list=$(bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#${checksum:0:-1}" [100,100])
+list=$(bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#${checksum:0:-1}" [99,99])
 echo $list | jq '.[0]'
