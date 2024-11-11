c2f945825aa487c46c6069e54c708340e5d4b2ba
Miguel-MCM <98604253+Miguel-MCM@users.noreply.github.com>
2024-11-11 14:18:27 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -3,4 +3,4 @@
 descinfo=$(bitcoin-cli getdescriptorinfo  "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)")
 checksum=$(echo $descinfo | jq .checksum | xargs)
 list=$(bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#${checksum:0:-1}" [99,99])
-echo $list | jq '.[0]'
+echo $list | jq '.[0]' | xargs
