fa49da7dc21e3a24a3f2bece403bcc3ab9c542c0
Miguel-MCM <miguelcarvalhodemedeiros.mcm@gmail.com>
2024-11-18 14:28:27 -0300

    006

--- submission/006.sh
+++ submission/006.sh
@@ -4,4 +4,4 @@ block1=$(bitcoin-cli getblock $hash1 2)
 hash2=$(bitcoin-cli getblockhash 257343)
 block2=$(bitcoin-cli getblock $hash2 2)
 coinbaseTx=$(echo $block1 | jq ".tx[] | select(.vin[] | has(\"coinbase\")).txid")
-echo $block2 | jq ".tx[] | select( .vin[].txid == $coinbaseTx).txid"
+echo $block2 | jq ".tx[] | select( .vin[].txid == $coinbaseTx)"
